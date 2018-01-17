b5903f727c7289b56e22ec5555cc508773238c66
Ricardo Eihara <ricardo.hejr@gmail.com>
2018-01-17 00:24:02 -0200

    Add invisible recaptcha

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,18 +1,23 @@
 class EmailsController < ApplicationController
+
   def create
-    # schedule email to send it
     @email = Email.new(email_params)
-    if @email.save
+    if verify_recaptcha(model: @email) && @email.save
       flash[:success] = 'Email created!'
       return render 'landing_page/index', status: :created
     end
-    flash[:error] = @email.errors.full_messages
-    redirect_to root_url
+
+    flash.now[:error] = @email.errors.full_messages
+    render 'landing_page/index', status: :unprocessable_entity
   end
 
   private
 
   def email_params
     params.require(:email).permit(:to, :body, :send_date_at)
+  end
+
+  def check_recaptcha
+
   end
 end
