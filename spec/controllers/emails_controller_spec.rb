9fbccfa877522eb4abd3b0d29278cca1a73e3d67
Ricardo Eihara <ricardo.hejr@gmail.com>
2018-04-02 22:11:22 -0300

    Add spec for confirm email action

--- spec/controllers/emails_controller_spec.rb
+++ spec/controllers/emails_controller_spec.rb
@@ -1,6 +1,61 @@
 require 'rails_helper'
 
 describe EmailsController, type: :controller do
+  describe 'GET #confirm_email' do
+    let(:email) { create(:email) }
+    let(:params) do
+      {
+        token: email.confirmation_token
+      }
+    end
+
+    describe 'successfully confirm the email' do
+      before { get :confirm_email, params: params }
+
+      it 'shows the notice message' do
+        expect(flash[:notice]).to be_present
+      end
+
+      it 'has a status code of 302' do
+        expect(response).to have_http_status :redirect
+      end
+    end
+
+    describe 'failed to confirm the email' do
+      context 'invalid token' do
+        let(:params) do
+          {
+            token: '12313'
+          }
+        end
+
+        before { get :confirm_email, params: params }
+
+        it 'shows the alert flash' do
+          expect(flash[:alert]).to be_present
+        end
+
+        it 'has a status code of 302' do
+          expect(response).to have_http_status :redirect
+        end
+      end
+
+      context 'when already confirmed' do
+        let!(:email) { create(:email, :confirmed) }
+
+        before { get :confirm_email, params: params }
+
+        it 'shows the alert flash' do
+          expect(flash[:alert]).to be_present
+        end
+
+        it 'has a status code of 302' do
+          expect(response).to have_http_status :redirect
+        end
+      end
+    end
+  end
+
   describe 'POST #create' do
     let(:email) { build(:email) }
 
@@ -15,9 +70,7 @@ describe EmailsController, type: :controller do
         }
       end
 
-      before do
-        post :create, params: params
-      end
+      before { post :create, params: params }
 
       it 'redirects to root path' do
         expect(response).to redirect_to(root_path)
@@ -35,9 +88,7 @@ describe EmailsController, type: :controller do
         }
       end
 
-      before do
-        post :create, params: params
-      end
+      before { post :create, params: params }
 
       it 'returns the status code' do
         expect(response).to have_http_status(:unprocessable_entity)
