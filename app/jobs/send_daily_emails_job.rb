8f04321223c97ab1e8e140ab153826be3c4c1bfb
Ricardo Eihara <ricardo.hejr@gmail.com>
2018-04-02 22:35:30 -0300

    Refactor send daily email job

--- app/jobs/send_daily_emails_job.rb
+++ app/jobs/send_daily_emails_job.rb
@@ -2,7 +2,7 @@ class SendDailyEmailsJob < ApplicationJob
   queue_as :default
 
   def perform
-    Email.today_emails.each do |email|
+    Email.today_emails.find_each do |email|
       DearMeMailer.dear_future_me(email).deliver_later
     end
   end
--- spec/jobs/send_daily_emails_job_spec.rb
+++ spec/jobs/send_daily_emails_job_spec.rb
@@ -1,24 +1,17 @@
 require 'rails_helper'
 
 describe SendDailyEmailsJob, type: :job do
-  describe '#perform' do
-    let(:email) { build(:email) }
+  let!(:email) { create(:email, send_date_at: Time.current) }
 
-    it 'calls on the DearMeMailer' do
-      allow(Email).to receive(:today_emails).and_return([email])
-      allow(DearMeMailer).to receive_message_chain(:dear_future_me, :deliver_now)
-
-      described_class.new.perform
-
-      expect(DearMeMailer).to have_received(:dear_future_me)
-    end
+  before do
+    allow(DearMeMailer).to receive(:dear_future_me).and_call_original
   end
 
-  describe '.perform_later' do
-    it 'adds the job to the queue' do
-      allow(DearMeMailer).to receive_message_chain(:dear_future_me, :deliver_now)
-      described_class.perform_later
-      expect(enqueued_jobs.last[:job]).to eq described_class
+  describe '#perform' do
+    it 'calls DearMeMailer' do
+      described_class.perform_now
+
+      expect(DearMeMailer).to have_received(:dear_future_me).with(email)
     end
   end
 end
